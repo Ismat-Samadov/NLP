d1d33e81c024405739c061654b6a1b7ecc213578
Ismat-Samadov <ismetsemedov@gmail.com>
2023-10-23 18:46:09 +0400

    check

--- app.py
+++ app.py
@@ -1,82 +1,48 @@
-import joblib
-import pandas as pd
-from flask import Flask, request, jsonify
-import numpy as np
-import warnings
-import requests
 import streamlit as st
+import requests
+API_URL = "https://home-price-predicition-209f2b8f62a1.herokuapp.com"
 
-# Suppress FutureWarning in Flask
-warnings.filterwarnings("ignore", category=FutureWarning)
+home_image_url = "https://images.pexels.com/photos/463734/pexels-photo-463734.jpeg?auto=compress&cs=tinysrgb&w=600"
 
-# Load the XGBoost model
-model = joblib.load('xgb.pkl')
-
-# Define the Flask API URL
-API_URL = "http://localhost:5000/predict"
-
-# Create a Streamlit app
-st.title("Real Estate Price Prediction App")
+image_html = f'<img src="{home_image_url}" style="max-width: 100%; height: auto;">'
+st.markdown(image_html, unsafe_allow_html=True)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 st.sidebar.title("Input Data")
-
-# Define a function to make predictions
-def make_prediction(data):
+st.title("Real Estate Price Prediction App")
+no_yes_mapping = {"No": 0, "Yes": 1}
+is_near_metro = st.sidebar.selectbox("Is Near Metro ?", ("No", "Yes"), format_func=lambda x: x)
+seller_type_encoded = st.sidebar.selectbox("Seller Type Encoded ?", ("No", "Yes"), format_func=lambda x: x)
+flat = st.sidebar.selectbox("Flat of home", list(range(1, 11)))
+total_flat = st.sidebar.slider("Total Flat of Building", 1, 25, 5)
+area_converted = st.sidebar.slider("Area of home in square meter:", 1, 100, 1)
+category_encoded = st.sidebar.selectbox("Category of building:", ("Old Building", "New Building"), format_func=lambda x: x)
+documents_encoded = st.sidebar.selectbox("Has documents ?", ("No", "Yes"), format_func=lambda x: x)
+is_repair_encoded = st.sidebar.selectbox("Is repaired ?", ("No", "Yes"), format_func=lambda x: x)
+data = {
+    "is_near_metro": no_yes_mapping[is_near_metro],
+    "seller_type_encoded": no_yes_mapping[seller_type_encoded],
+    "flat": flat,
+    "total_flat": total_flat,
+    "area_converted": area_converted,
+    "category_encoded": 0 if category_encoded == "Old Building" else 1,
+    "documents_encoded": no_yes_mapping[documents_encoded],
+    "is_repair_encoded": no_yes_mapping[is_repair_encoded],
+}
+if st.sidebar.button("Predict"):
     try:
-        response = requests.post(API_URL, json=data)
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(API_URL + "/predict", json=data, headers=headers)
         if response.status_code == 200:
             result = response.json()
-            return result['predicted_price']
+            predicted_price = int(result['predicted_price'])
+            st.sidebar.success(f"Predicted Price: {predicted_price}")
         else:
-            return None
+            st.sidebar.error("Prediction request failed.")
     except Exception as e:
-        return None
-
-# Create input fields for user data in the Streamlit sidebar
-is_near_metro = st.sidebar.number_input("Is Near Metro (0 or 1):", min_value=0, max_value=1, step=1)
-seller_type_encoded = st.sidebar.number_input("Seller Type Encoded (0 or 1):", min_value=0, max_value=1, step=1)
-flat = st.sidebar.number_input("Flat:", value=2)
-total_flat = st.sidebar.number_input("Total Flat:", value=5)
-area_converted = st.sidebar.number_input("Area (converted):")
-category_encoded = st.sidebar.number_input("Category Encoded (0 or 1):", min_value=0, max_value=1, step=1)
-documents_encoded = st.sidebar.number_input("Documents Encoded (0 or 1):", min_value=0, max_value=1, step=1)
-is_repair_encoded = st.sidebar.number_input("Is Repair Encoded (0 or 1):", min_value=0, max_value=1, step=1)
-
-# Make a prediction when the "Predict" button is clicked
-if st.sidebar.button("Predict"):
-    data = {
-        "is_near_metro": is_near_metro,
-        "seller_type_encoded": seller_type_encoded,
-        "flat": flat,
-        "total_flat": total_flat,
-        "area_converted": area_converted,
-        "category_encoded": category_encoded,
-        "documents_encoded": documents_encoded,
-        "is_repair_encoded": is_repair_encoded,
-    }
-    
-    predicted_price = make_prediction(data)
-    if predicted_price is not None:
-        st.sidebar.success(f"Predicted Price: {predicted_price:.2f}")
-    else:
-        st.sidebar.error("Prediction request failed.")
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-
-    @app.route('/predict', methods=['POST'])
-    def predict():
-        try:
-            data = request.get_json()
-            feature_names = [
-                'is_near_metro', 'seller_type_encoded', 'flat', 'total_flat', 'area_converted',
-                'category_encoded', 'documents_encoded', 'is_repair_encoded'
-            ]
-            df = pd.DataFrame([data], columns=feature_names)
-            df = df.applymap(lambda x: float(x) if isinstance(x, np.float32) else x)
-            predicted_price = model.predict(df)
-            response = {'predicted_price': float(predicted_price[0])}
-            return jsonify(response)
-        except Exception as e:
-            return jsonify({'error': str(e)})
-    
-    app.run(debug=False)  # Disable Flask debugging when using Streamlit
+        st.sidebar.error(f"An error occurred: {str(e)}")
